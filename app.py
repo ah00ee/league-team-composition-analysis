e277fde357b237990151ef5c15deccfb1bc9a50f
노아영 <nhoahyoung@noayeong-ui-MacBookAir.local>
2021-08-26 00:54:09 +0900

    commit

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 import flask
-from flask import Flask, request
+from flask import Flask, request, url_for
 import json
 import requests
 
 app = Flask(__name__)
-
+'''
 @app.route('/')
 def home():
     return flask.render_template('index.html')
+'''
+@app.route('/')
+def search():
+    return flask.render_template('summoner_search.html')
 
-@app.route('/data', methods=['GET', 'POST'])
+@app.route('/data', methods=['GET'])
 def get():
-    apiKey = 'RGAPI-8edf16af-d2b5-4fff-ab58-ffcad9d6e239'
-    summonerName = ''
+    summonerName = request.args.get('summoner_name')
+    apiKey = 'Your Riot API KEY'
+    url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={apiKey}'
+    summonerInfo = json.loads(requests.get(url).text)
 
-    if request.method == 'GET':
-        return flask.render_template('summoner_search.html')
-    elif request.method == 'POST':
-        summonerName = request.form['summoner_name']
-        url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={apiKey}'
-        summonerInfo = json.loads(requests.get(url).text)
-        return flask.render_template('summoner_info.html', data=summonerInfo)
-
-    # https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/Colleen
+    return flask.render_template('summoner_info.html', data=summonerInfo)    
 
 if __name__ == '__main__':
     app.run()
